6f887bc0b2ca9c77ad02b4381d57f6e87afc1aff
sailorpiecescript-server <vietrolboxacc1@gmail.com>
2026-04-02 21:51:59 +0800

    Update Loader.lua

--- SailorPiece/Loader.lua
+++ SailorPiece/Loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/hB4gmFDYLG/0"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/UyZlizk2sV/0"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sailorpiecescript-server/SailorPiecex1/refs/heads/main/source"))()
