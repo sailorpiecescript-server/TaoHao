3f9a7e7f6e69a92f2687384c0f5aa2d1718d9d45
sailorpiecescript-server <vietrolboxacc1@gmail.com>
2026-04-22 12:01:03 +0800

    Update Loader.lua

--- SailorPiece/Loader.lua
+++ SailorPiece/Loader.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Hdxu6tcTEz/0"))()
+
+loadstring(game:HttpGet("https://rawscripts.net/raw/2x-Luck-Sailor-Piece-TaoLao-Source-Code-Fixed-146640"))()
